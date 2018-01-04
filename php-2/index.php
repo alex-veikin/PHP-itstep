00f748e93146a90b7c4421017a91ca71c3fc7778
alex-veikin <vejkin@yandex.ru>
2018-01-05 01:29:03 +0300

    Edit php-2 at home

--- php-2/index.php
+++ php-2/index.php
@@ -18,23 +18,32 @@
 
 <?php
 
+//Функция, принимающая массив строк и выводящая каждую строку в отдельном параграфе.
 function arrayToStrings($arr) {
     foreach ($arr as $str) {
-        echo "<p>$str</p>\n";
+        echo "<p>$str</p>";
     }
 }
 
-
-$arrStr = ["This some text", "DFklasfdkda dslkfk dslfksd", "Ksdkfsdf dfs sdfsfd sdf"];
+$arrStr = ["First text", "Second text", "Third text"];
 arrayToStrings($arrStr);
 
-echo "<br>";
+echo "<hr>";
 
 
 
+//Функция, принимающая 2 параметра ­ массив чисел и строку,
+//обозначающую арифметическое действие,
+//которое нужно выполнить со всеми элементами массива
 function math($arrNum, $op) {
-    $res = 0;
+	$res = 0;
+
     foreach ($arrNum as $num) {
+        if (!$res) {
+            $res = $num;
+            continue;
+        }
+
         switch ($op) {
             case "+":
                 $res += $num;
@@ -50,19 +59,29 @@ function math($arrNum, $op) {
                 break;
         }
     }
+
     echo "<p>$res</p>";
 }
 
-math([2, 3, 5], "-");
+math([2, 3, 5], "*");
 
-echo "<br>";
-
-
+echo "<hr>";
 
 
+
+//Функция, принимающая переменное число аргументов,
+//но первым аргументов обязательно должна быть строка,
+//обозначающее арифметическое действие,
+//которое необходимо выполнить со всеми передаваемыми аргументами.
 function math2($op, ...$args) {
     $res = 0;
+
     foreach ($args as $num) {
+	    if (!$res) {
+		    $res = $num;
+		    continue;
+	    }
+
         switch ($op) {
             case "+":
                 $res += $num;
@@ -78,18 +97,29 @@ function math2($op, ...$args) {
                 break;
         }
     }
+
     echo "<p>$res</p>";
 }
 
-math2("+", 2, 5, 6);
+math2("+", 2, 5, 6, 8, 3);
 
+echo "<hr>";
+
+
+
+//Функция принимающая два параметра ­ 2 целых числа.
+//Если вводятся не 2 целых числа ­ то функция должна выводить ошибку на экран.
+//Если пользователь вводит 2 целых числа ­ то ему должна отрисовываться таблица умножения
+//размером со значения параметров, переданных функции.
+echo "<p>Функция принимающая два параметра - 2 целых числа. " .
+    "Если вводятся не 2 целых числа - то функция должна выводить ошибку на экран. " .
+    "Если пользователь вводит 2 целых числа - то ему должна отрисовываться таблица умножения " .
+    "размером со значения параметров, переданных функции.</p>";
 echo "<br>";
-
-
-
 
 function table($a, $b) {
     $res = "<table>";
+
     for ($i = 1; $i <= $b; $i++) {
         $res .= "<tr>";
         for ($j = 1; $j <= $a; $j++) {
@@ -101,12 +131,114 @@ function table($a, $b) {
         }
         $res .= "</tr>";
     }
+
     $res .= "</table>";
+
     echo $res;
 }
 
-table(4, 3);
+$table_a = $_POST['table_a'];
+$table_b = $_POST['table_b'];
 
+?>
+
+<form action="#" method="post">
+    <label>Введите первое число:
+        <input type="text" name="table_a" value="<?= $table_a ?>">
+    </label>
+    <label>Введите второе число:
+        <input type="text" name="table_b" value="<?= $table_b ?>">
+    </label>
+    <input type="submit" name="submit" value="Ok">
+</form>
+
+<?php
+
+if (!$table_a || !$table_b) {
+	echo "<p class='alert'>Введите данные.</p>";
+} else if (ctype_digit($table_a) && ctype_digit($table_b)) {
+	table(intval($table_a), intval($table_b));
+} else {
+    echo "<p class='alert'>Некорректно введены данные. \n Введите целые числа.</p>";
+}
+
+echo "<hr>";
+
+
+
+//Функция, принимающая в качестве аргумента массив чисел вида: 1, 22, 5, 66, 3, 57
+//и возвращает массив по возрастанию: 1, 3, 5, 22, 57, 66
+function sortArr($arr) {
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        for ($j = 0; $j < count($arr) - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $arr;
+}
+
+$my_arr = [1, 22, 5, 66, 3, 57, 8, 2, 28];
+
+print_r($my_arr);
+echo "<br>";
+print_r(sortArr($my_arr));
+
+echo "<hr>";
+
+
+
+//Рекурсивную функцию, принимающую два целых числа ­ начальное значение и конечное значение.
+//Например, первый аргумент 10, второй ­ 35.
+//Функция должна вывести на экран список нечетных чисел от 10 до 35.
+function getOdd($a, $b) {
+    if ($a > $b) return;
+	if ($a % 2) echo $a . "<br>";
+
+	getOdd($a + 1, $b);
+}
+
+getOdd(10, 35);
+
+echo "<hr>";
+
+
+
+//Функция, получающая 1 параметр (строку) и возвращающая TRUE,
+//если строка является палиндромом, в противном случае - FALSE.
+function isPalindrome($str) {
+    $str = preg_replace("/[^a-zA-ZА-Яа-яЁё0-9]/u","",$str);
+    $str = mb_strtolower($str);
+    $half = floor(mb_strlen($str) / 2);
+    $strRev = iconv('utf-8', 'utf-16le', $str);
+	$strRev = iconv('utf-16be', 'utf-8', strrev($strRev));
+
+    return mb_substr($str, 0, $half) === mb_substr($strRev, 0 ,$half);
+}
+
+$str1 = "Abc d  cba";
+$str2 = "А роза упала на лапу Азора";
+$str3 = "Madam, I'm Adam";
+
+echo $str1;
+echo "<br>";
+var_dump(isPalindrome($str1));
+echo "<br>";
+echo "<br>";
+
+echo $str2;
+echo "<br>";
+var_dump(isPalindrome($str2));
+echo "<br>";
+echo "<br>";
+
+echo $str3;
+echo "<br>";
+var_dump(isPalindrome($str3));
 
 ?>
 
